3658ad67b78547ddad57844c9e40458e4757f3e2
alexsix6 <six.alex81@gmail.com>
2025-06-10 00:51:54 -0500

    Fix analyze-idea.js syntax - Corrige sintaxis CommonJS para compatibilidad Vercel

--- api/analyze-idea.js
+++ api/analyze-idea.js
@@ -1,6 +1,6 @@
 const openRouterClient = require('../lib/openrouter-client');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     console.log('🧠 [Analyze Idea] Request received');
     
     if (req.method !== 'POST') {
